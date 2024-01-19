42079af74ef54d17352732dcddfa6d75f5fc500d
unknown <dagihadgu24@gmail.com>
2024-01-19 09:35:27 -0800

    first commit

---
+++ blogapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+]
---
+++ blogapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+# blog/views.py
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blogapp/post_list.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'blogapp/about.html')
+
+def contact(request):
+    return render(request, 'blogapp/contact.html')
